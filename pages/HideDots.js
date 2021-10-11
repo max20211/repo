7d7d6ea7114be250ee23833cfb4f5771a92a06bc
max20211 <89989805+max20211@users.noreply.github.com>
2021-10-11 15:02:03 -0400

    Added repo and site

---
+++ pages/HideDots.js
@@ -0,0 +1,9 @@
+import Depiction from '../components/Depiction'
+
+export default function HideDots() {
+	return (
+		<Depiction name="HideDots" github="https://github.com/Chr1sDev/hidedots" subtitle="Hide page dots">
+			Hide page dots on iOS 14.
+		</Depiction>
+	)
+}
---
+++ pages/api/Packages.js
@@ -0,0 +1,40 @@
+import { packages, icons } from '../../loader!../../repo'
+import { getRepoUrl } from '../../utils'
+
+const spaceRegExp = /\s/g
+
+export default (req, res) => {
+	const url = getRepoUrl(req)
+	const result = []
+	let sileoDepictions = {
+		discord_video_loop: `${url}SileoDepictions/discord-video-loop.json`,
+		ChatWall_Beta: `${url}SileoDepictions/ChatWall-Beta.json`,
+		NoNewContactPhotoAvailableBanner: `${url}SileoDepictions/NoNewContactPhotoAvailableBanner.json`,
+		NewTab: `${url}SileoDepictions/NewTab.json`,
+		TapFlash: `${url}SileoDepictions/TapFlash.json`,
+		Current: `${url}SileoDepictions/Current.json`,
+	}
+	for (const name in packages) {
+		const versions = packages[name]
+		for (const version in versions) {
+			const p = versions[version] // package is a reserved variable nametap
+			const strings = []
+			for (const entry in p.meta) {
+				strings.push(`${entry}: ${p.meta[entry]}`)
+			}
+
+			icons[name.replace(spaceRegExp, '_')] &&
+				strings.push(`Icon: ${url}${icons[name.replace(spaceRegExp, '_')]}`)
+			strings.push(`Depiction: ${url}${name.replace(spaceRegExp, '-')}`)
+
+			if (sileoDepictions[name.replace(spaceRegExp, '_')]) {
+				strings.push(`SileoDepiction: ${sileoDepictions[name.replace(spaceRegExp, '_')]}`)
+			}
+
+			result.push(strings.join('\n'))
+		}
+	}
+
+	res.setHeader('Cache-Control', 's-maxage=31536000')
+	res.end(result.join('\n\n'))
+}
